c765ef56b57cfd6b4c767cc1d6ad17fabccf2b19
BerkanGH <berkankeskin28@gmail.com>
2024-11-16 06:49:51 +0300

    odev

---
+++ 04Join/04Join/Books.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04Join
+{
+    public class Books
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+
+        public int  AuthorId { get; set; }
+
+       
+    }
+
+}
---
+++ 04Join/04Join/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using _04Join;
+class Program
+{
+    static void Main(string[] args)  // Ana metodun adı Main olmalı
+    {
+        // Yazarlar listesi
+        List<Author> Yazarlar = new List<Author>()
+        {
+            new Author { AuthorId = 1, Name = "Steve Erikson" },
+            new Author { AuthorId = 2, Name = "J.R.R. Tolkien" },
+            new Author {AuthorId=3, Name= "Dave Gurney"}
+        };
+        
+        //Kitaplar Listesi
+       
+        List<Books> kitaplar = new List<Books>()
+        {
+            new Books {AuthorId = 1, BookId = 1, Title="Ölühane Kapıları"},
+            new Books {AuthorId=2, BookId=2, Title = "Yüzüklerin Efendisi"},
+            new Books {AuthorId=1, BookId=3, Title="Ay Bahçeleri"},
+            new Books {AuthorId=2, BookId=4, Title= "Hobbit"},
+            new Books {AuthorId=3, BookId =5, Title = "Aklından Bir Sayı Tut"}
+
+        };
+
+        // join ile author id leri birleştiriyoruz
+
+        var Birleştirme = Yazarlar.Join(kitaplar,
+            x => x.AuthorId,
+            y => y.AuthorId,
+            (x, y) => new
+            {
+                AuthorName = x.Name,
+                BookName = y.Title
+            }
+            );
+
+        // Burdada yazdırıyoruz
+        foreach (var author in Birleştirme)
+        {
+            Console.WriteLine($"Yazar: {author.AuthorName}, Kitap adı: {author.BookName}");
+        }
+    }
+}
